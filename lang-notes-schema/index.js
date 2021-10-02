73807d96b9e2434aceb9a3145d6ace467d315b19
Joshua Enokson <kilograhm@pm.me>
2021-10-02 11:10:46 +0500

    added a shared schema pkg

---
+++ lang-notes-schema/index.js
@@ -0,0 +1,5 @@
+import * as tables from './tables'
+
+module.exports = {
+    tables
+}
---
+++ lang-notes-schema/tables.js
@@ -0,0 +1,97 @@
+const defintionGroupsRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        name: { type: 'string' }
+    }
+}
+
+const languagesRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        name: { type: 'string' }
+    }
+}
+
+const translationsRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        langId: { type: 'number', "$comment": 'this prop correlates to the id of the languages table' },
+        definition: { type: 'string' },
+        // literal: { type: 'string' },
+        misc: { type: 'string' }
+    }
+}
+
+const searchKeysRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        definitionId: { type: 'number' },
+        key: { type: 'string' }
+    }
+}
+
+const examplesRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        definitionId: { type: 'number' },
+        translationId: { type: 'number' },
+        example: { type: 'string' }
+    },
+    required: [ "id", "example" ]
+}
+
+const definitionsRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' },
+        groupId: { type: 'number' },
+        clusterId: { type: 'number' },
+        pronounciation: { type: 'string' },
+        word: { type: 'string' },
+        prefixes: { type: 'string' },
+        suffixes: { type: 'string' },
+        definition: { type: 'string' }
+    }
+}
+
+const clusterRow = {
+    type: 'object',
+    properties: {
+        id: { type: 'number' }
+    }
+}
+
+const translationSchema = {
+    type: 'object',
+    properties: {
+        translation: translationsRow,
+        lang: languagesRow,
+        examples: { type: 'array', items: examplesRow }
+    }
+}
+
+const definitionSchema = {
+    type: 'object',
+    properties: {
+        definition: definitionsRow,
+        group: defintionGroupsRow,
+        translations: { type: 'array', items: translationSchema },
+        searchKeys: { type: 'array', items: searchKeysRow },
+        examples: { type: 'array', items: examplesRow }
+    }
+}
+
+const clusterSchema = { 
+    type: 'object',
+    properties: {
+        cluster: clusterRow,
+        definitions: { type: 'array', items: definitionSchema }
+    }
+}
+
+const clustersSchema = { type: 'array', items: clusterSchema }
